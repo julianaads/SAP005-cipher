ec0c12d40db67d34dc4a4d6aa3e4b29c79378e9b
Juliana <juliana.souzas@yahoo.com.br>
2020-11-02 18:48:26 -0300

    Adicao da funcao Criptografar

--- index.js
+++ index.js
@@ -1,68 +1,83 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js'
 
-     
-  
-
-     
 //Códigos relacionados ao formulário Criptografar
       
 
-//primeiro botão
+//botão criptografar
 const startCripto = document.getElementById("startCripto").addEventListener("click",criptografarMsg);
+//tranformar o conteudo do criptografar em letras maiusculas
 const caixaAlta = document.getElementById("criptografar").addEventListener("change",criptoMaiuscula);
 
 
-   //função para deixar letra maiúscula Criptografar
 
+//função para deixar letra maiúscula Criptografar
    function criptoMaiuscula(){
-        console.log("Estou funcionando?");
+        //console.log("Estou funcionando?"); teste para saber se estava funcionando
         const converTexto = document.getElementById("criptografar").value;
         document.getElementById("criptografar").value = converTexto.toUpperCase();
-   
   }
 
-
+  //variaveis dos formulários
+  
+  
 
 // função para o primeiro botão
-function criptografarMsg() {
+function criptografarMsg(event) {
+    //preventDefault serve para minha pag não atualizar quando clico no botão
+    event.preventDefault();
+    const alfabeto = 26;
+    const codAsc = 65;
     const msgCripto = document.getElementById("criptografar").value;
-    const criptOffset = (document.getElementById("criptOffset")).value;
-    const resultCripto = document.getElementById("resultCripto");
+    const criptOffset = parseInt(document.getElementById("criptOffset").value);
+    let resultCripto = "";
+
+    for(let i=0; i < msgCripto.length; i++){
+        
+        const criptografar = ((msgCripto.charCodeAt(i) - codAsc + criptOffset) % alfabeto) + codAsc;
     
-    if (msgCripto == "" && criptOffset == "") {
+        resultCripto += String.fromCharCode(criptografar)
+        //resultCripto = console.log(resultCripto);
 
-        resultCripto.textContent = "Preencha todos os Campos"
-    }
-
-
+        document.getElementById("resultCripto").innerHTML = resultCripto
+        //return resultCripto
+    
+      }
+    
+      
 }
+
+
+
+
+
 
 //códigos relacionados ao formulário Descriptografar 
 //segundo botão
 const startDescripto = document.getElementById("startDescripto").addEventListener("click",descriptografarMsg);
 const maiuscula = document.getElementById("descriptografar").addEventListener("change",descriptoMaiuscula);
 
-
 //função para deixar letras maiusculas
 function descriptoMaiuscula() {
-    console.log("Está ai?");
     const textoConverte = document.getElementById("descriptografar").value;
     document.getElementById("descriptografar").value = textoConverte.toUpperCase();
 
 }
 
 
-
 //função segundo botão
-function descriptografarMsg() {
+function descriptografarMsg(event) {
     const msgDescripto = document.getElementById("descriptografar").value;
     const descriptOffset = (document.getElementById("descriptOffset")).value;
     const resultDescripto = document.getElementById("resultDescripto");
-
+       
+    //preventDefault serve para minha pag não atualizar quando eu clico no botão
+    event.preventDefault();
+    
     if (msgDescripto == "" && descriptOffset == "") {
         
         resultDescripto.textContent = "Preencha todos os Campos"
-
+    }else{
+        resultDescripto.textContent = msgDescripto
     }
        
 
@@ -71,6 +86,8 @@ function descriptografarMsg() {
 
 
 
-console.log(cipher); 
+console.log(cipher);
+
+
 
 
